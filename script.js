0417ebe1635eeaef1006486f42a21556845b1f06
qamarbasim7020-prog <qamarbasim702@gmail.com>
2025-12-23 00:26:08 +0500

    Added 3 portfolio images, updated footer links, fixed CSS and JS

--- script.js
+++ script.js
@@ -83,4 +83,15 @@ if(contactForm){
     alert('Thank you! Your message has been received.');
     contactForm.reset();
   });
-}
+}const fades = document.querySelectorAll('.fade');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, { threshold: 0.2 });
+
+fades.forEach(fade => observer.observe(fade));
+
